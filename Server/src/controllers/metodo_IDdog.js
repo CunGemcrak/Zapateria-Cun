b321d6722f1fc91adc8acabda767d02a63677f94
CunGemcrak <142614397+CunGemcrak@users.noreply.github.com>
2024-06-08 08:42:46 -0500

    Creacion de Carpeta Principal

---
+++ Server/src/controllers/metodo_IDdog.js
@@ -0,0 +1,52 @@
+const { Dog, Temperaments} = require('../db.js')
+const axios = require('axios')
+const {URL2, APPI_KEY} = process.env;
+
+
+const dog = async (req, res) =>{
+    const idRaza = req.params.idRaza
+
+    //console.log("Esta es la id Raza: "+idRaza);
+    
+    try {
+        if (Number.isNaN(Number(idRaza))) {
+           // console.log("Entro al id con api");
+            const respinse = await axios.get(`${URL2}${idRaza}?api_key=${APPI_KEY}`)//`${URL2}/${idRaza}?api_key=${APPI_KEY}`)    
+            let datos = respinse.data
+           // const bd_dogs = Dog.findAll()
+            //console.log("Direccion de Acceso"+JSON.stringify(datos))
+            //
+           // console.log("Solo API")
+            return res.status(200).json({datos})
+           
+        }else{
+           
+
+            console.log("Entro al id con numero");
+            const bd_dog = await Dog.findByPk(idRaza, {
+                include: Temperaments
+            });
+
+            if (bd_dog) {
+                let datos = bd_dog
+             //   console.log("que tengo del db_dog" + JSON.stringify(datos))
+                return res.status(200).json({ datos });
+            } else {
+                return res.status(404).json({ message: 'No se encontró el perro con el ID proporcionado' });
+            }
+
+        }
+       
+        
+    
+    } catch (error) {
+            console.error(error)
+            return res.status(500).json({message: error.message})
+        } 
+
+}
+module.exports = {
+    
+    dog,
+    
+}
---
+++ Server/src/routes/index.js
@@ -0,0 +1,33 @@
+const { Router } = require('express');
+// Importar todos los routers;
+// Ejemplo: const authRouter = require('./auth.js');
+
+
+// import metodos controles een un solo documento 
+//const {allDogs, dog, dogName, Temperamento} = require('../controllers/metodosGet')
+const {busquedaDog} = require('../controllers/metodosPost')
+const {allDogs}= require('../controllers/metodo_Alldogs')
+const {dog}= require('../controllers/metodo_IDdog')
+const {dogName}= require('../controllers/metodo_NameDog')
+const {Temperamento}= require('../controllers/metodo_Temperament')
+
+
+
+//import de los moduloes o controler por separado
+
+
+const router = Router();
+
+// Configurar los routers
+// Ejemplo: router.use('/auth', authRouter);
+
+
+
+router.get('/dogs', allDogs);
+router.get('/dogs/:idRaza', dog);
+router.get('/dogs/name/:name', dogName);
+router.get('/temperament', Temperamento);
+router.post('/dogs', busquedaDog);
+
+
+module.exports = router;
