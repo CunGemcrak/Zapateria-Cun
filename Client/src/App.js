1a1a803a001924a1254a2c6930392ca72ea69846
CunGemcrak <142614397+CunGemcrak@users.noreply.github.com>
2024-06-08 18:22:44 -0500

    Actualizacion de datos

--- Client/src/App.js
+++ Client/src/App.js
@@ -5,15 +5,15 @@ import { Routes,Route} from "react-router-dom";
 
 
 import Loading from './Component/loading/Loading';
-import Login from './Component/Login/Login'
+import Login from './Component/Login/Login_General/Login_General'
 
 
 function App() {
   return (
     <div className="App">
       <Routes>
-              <Route path='/' element={<Loading/>}/>
               <Route path='/login' element={<Login/>}/>
+              <Route path='/' element={<Loading/>}/>
      
         
       </Routes>
