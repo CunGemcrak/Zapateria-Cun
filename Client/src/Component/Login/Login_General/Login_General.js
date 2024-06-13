63901854f4a662da156097de81f1d8df5a0b7aad
Matekjimenezh <mateojimenez200244@gmail.com>
2024-06-12 21:07:02 -0400

    Mateo actualizo

--- Client/src/Component/Login/Login_General/Login_General.js
+++ Client/src/Component/Login/Login_General/Login_General.js
@@ -1,11 +1,58 @@
 
-const Login = () => {
-    
-    return (
-        <div className="contenedor_Login">
-            DEsarrollo del login
-        </div>
-    );
-}
+import React, { useState } from 'react';
+import { FaEye, FaEyeSlash } from 'react-icons/fa'; // Importamos los íconos de ojo abierto y cerrado
 
-export default Login;
+const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Aquí podrías agregar la lógica para enviar los datos de inicio de sesión al servidor
+    console.log('Usuario:', username);
+    console.log('Contraseña:', password);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Usuario:</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={handleUsernameChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Contraseña:</label>
+        <input
+          type={showPassword ? 'text' : 'password'}
+          id="password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+        <button type="button" onClick={togglePasswordVisibility}>
+          {showPassword ? <FaEyeSlash /> : <FaEye />}
+        </button>
+      </div>
+      <button type="submit">Iniciar sesión</button>
+    </form>
+  );
+};
+
+export default LoginForm;
+
