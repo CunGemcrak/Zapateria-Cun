be46a17b1a3904739b315a7a545ca25abe602030
RIOSCA <cristian.riosc@cun.edu.co>
2024-06-22 18:33:22 -0500

    perfiluser

--- Client/src/App.js
+++ Client/src/App.js
@@ -5,6 +5,7 @@ import { Routes,Route} from "react-router-dom";
 import LoginPrincipal from './Component/Login/Login_Principal/LoginPrincipal';
 import Loading from './Component/loading/Loading';
 import Home from './Component/home/Home';
+import PerfilUser from './Component/Usuario/Perfil_User/Perfil_User';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
               <Route path='/'  element={<Loading/>}/>
               <Route path='/login' element={<LoginPrincipal/>}/>
               <Route path='/home' element={<Home/>}/>
+              <Route path='/perfiluser' element={<PerfilUser/>}/>
              
              
       </Routes>
--- Client/src/Component/Usuario/Perfil_User/Perfil_User.js
+++ Client/src/Component/Usuario/Perfil_User/Perfil_User.js
@@ -1,6 +1,41 @@
+import React from 'react';
+import "./Perfil_user.css"
+
 const PerfilUser =()=>{
     return <>
-    perfil del usuario
+        <body>
+      <from>
+        <h2>PERFIL USUARIO</h2>
+        <div class="input-gruop">
+          <div class="input-container">
+            <input type='text' name='nombre' placeholder='Nombre'></input>
+            
+          </div>
+          <div class="input-container">
+            <input type='numero' name='N° documento' placeholder='N° Documento'></input>
+            
+          </div>
+          <div class="input-container">
+            <input type='email' name='correo' placeholder='Correo'></input>
+            
+          </div>
+          <div class="input-container">
+            <input type='telefono' name='telefono' placeholder='Telefono'></input>
+            
+          </div>
+          <div class="button">
+            <button>Editar</button>
+
+          </div>
+          
+
+
+        </div>
+
+
+      </from>
+    </body>
+
     </>
 }
 export default PerfilUser;
